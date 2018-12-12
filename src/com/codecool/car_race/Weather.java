417679755432bd7351873f2b155129322cb55ef3
AndrasHorinka <horinka.andras@gmail.com>
2018-12-12 11:38:12 +0100

    Weather class

--- src/com/codecool/car_race/Weather.java
+++ src/com/codecool/car_race/Weather.java
@@ -1,4 +1,13 @@
 package com.codecool.car_race;
 
 public class Weather {
+    private boolean rain = false;
+
+    public boolean isRaining() {
+        return this.rain;
+    }
+
+    public void setWeather(boolean rain) {
+        this.rain = rain;
+    }
 }
