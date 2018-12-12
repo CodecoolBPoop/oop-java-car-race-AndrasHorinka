5bb8aa21402018c5403ffe8e0f9844480a84f19e
AndrasHorinka <horinka.andras@gmail.com>
2018-12-12 11:43:46 +0100

    init Motorcycle class

--- src/com/codecool/car_race/Motorcycle.java
+++ src/com/codecool/car_race/Motorcycle.java
@@ -2,3 +2,15 @@ package com.codecool.car_race;
 
 public class Motorcycle {
 }
+
+/*
+    Motorcycle // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly). Doesn't care about trucks.
+            motorcycleNumber // The number of the instance created. Used for its name.
+
+    name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
+            distanceTraveled // holds the summarized distance traveled in the race.
+
+    prepareForLap(Race race) // setup the actual speed used for the current lap
+    moveForAnHour() // The vehicle travels for an hour. It increases the distance traveled. Call this from the Race::simulateRace() only!
+
+    */
