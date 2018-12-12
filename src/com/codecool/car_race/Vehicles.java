5459597001cea641fe5559b4b6078140a366021a
AndrasHorinka <horinka.andras@gmail.com>
2018-12-12 11:38:39 +0100

    Vehicles interface

--- src/com/codecool/car_race/Vehicles.java
+++ src/com/codecool/car_race/Vehicles.java
@@ -1,4 +1,31 @@
 package com.codecool.car_race;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Vehicles {
+    int originalSpeed = 0;
+    int actualSpeed = 0;
+    int totalDistanceTravelled = 0;
+    List<Integer> distancePerRound = new ArrayList<>();
+    String name = null;
+
+
+    void prepareForLap(Race race);
+
+    void moveForAnHour();
+
+    void setName();
+            /* call random to pick a nem from firstName and lastName
+        concatenate them
+         */
+
+    void calculateDistance();
+
+    void setDistancePerRound();
+
+
+
+
+
 }
